447c1c70a75e71f4657014ebaa26f7400860de5d
isooosi <137146930+isooosi@users.noreply.github.com>
2023-12-19 02:56:48 +0000

    update rss2.0

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,23 +1,23 @@
-import rss from '@astrojs/rss'
-import { getCollection } from 'astro:content'
+import rss from '@astrojs/rss';
+import { getCollection } from 'astro:content';
 
-export async function get() {
-	let posts = await getCollection('posts')
+export async function GET(context) {
+    let posts = await getCollection('posts');
+    
+    posts.sort((a, b) => new Date(b.data.pubDate) - new Date(a.data.pubDate));
 
-	posts.sort((a, b) => new Date(b.data.pubDate) - new Date(a.data.pubDate))
+    posts = posts.slice(0, 3);
 
-	posts = posts.slice(0, 3)
-
-	return rss({
-		title: 'VisVrs: An Astro Theme for Visual Storytelling',
-		description: 'VisVrs is an Astro theme specifically designed for photo and text blogging, making it ideal for blog authors and content creators.',
-		site: 'https://visvrs.vercel.app',
-		items: posts.map((post) => ({
-			title: post.data.title,
-			pubDate: post.data.pubDate,
-			description: post.data.description,
-			link: `${post.slug}/`
-		})),
-		customData: `<language>en</language>`
-	})
+    return rss({
+        title: 'VisVrs: An Astro Theme for Visual Storytelling',
+        description: 'VisVrs is an Astro theme specifically designed for photo and text blogging, making it ideal for blog authors and content creators.',
+        site: context.site,
+        items: posts.map((post) => ({
+            title: post.data.title,
+            pubDate: post.data.pubDate,
+            description: post.data.description,
+            link: `${post.slug}/`
+        })),
+        customData: `<language>en</language>`
+    });
 }
