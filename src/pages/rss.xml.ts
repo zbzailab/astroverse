1be3ca16a0038b865e9a94b3f8c5b5013c25154d
isooosi <137146930+isooosi@users.noreply.github.com>
2024-01-09 02:46:36 +0000

    Output images in RSS.

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -16,8 +16,18 @@ export async function GET(context) {
             title: post.data.title,
             pubDate: post.data.pubDate,
             description: post.data.description,
-            link: `${post.slug}/`
+            link: `${post.slug}/`,
+            customElements: [
+                {
+                    name: 'media:content',
+                    attributes: {
+                        url: post.data.cover, // 图片的URL，根据你的数据结构来选择正确的字段
+                        medium: 'image',
+                        type: 'image/jpeg', // 图片的类型，根据你的需要选择合适的类型
+                    },
+                },
+            ],
         })),
-        customData: `<language>en</language>`
+        customData: `<language>zh-cn</language>`
     });
 }
