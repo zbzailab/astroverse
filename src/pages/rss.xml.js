b0f7333e240ee6302e02851686ee12bdea7a33d1
penboxlab <penboxlab@gmail.com>
2025-02-03 17:37:44 +0800

    - Updated project to Astro 5.2 for improved performance and new features. - Replaced static last modified time with dynamic Git commit timestamp to reflect the actual last modification date of content. - Optimized RSS feed generation by:   - Adding Markdown-It to convert markdown content to HTML.   - Using sanitize-html to ensure content is sanitized, allowing only necessary HTML tags (e.g., img, h1, h2).   - Enhanced RSS item structure for cleaner and more efficient output.   - Updated feed metadata and improved feed formatting for better compatibility and SEO.

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,8 +1,6 @@
 import rss from "@astrojs/rss";
 import { getCollection } from "astro:content";
 import { SITE_TITLE, SITE_DESCRIPTION } from "@consts";
-import MarkdownIt from 'markdown-it';
-import sanitizeHtml from 'sanitize-html';
 
 export async function GET(context) {
   let posts = await getCollection("posts");
@@ -10,8 +8,6 @@ export async function GET(context) {
   posts = posts
     .sort((a, b) => new Date(b.data.pubDate) - new Date(a.data.pubDate))
     .slice(0, 3);
-
-  const markdown = new MarkdownIt();
 
   return rss({
     title: SITE_TITLE,
@@ -24,9 +20,7 @@ export async function GET(context) {
       description: post.data.description,
       link: `/posts/${post.slug}`,
       pubDate: post.data.pubDate,
-      content: sanitizeHtml(markdown.render(post.body), {
-        allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img', 'h1', 'h2']),
-      }),
+      content: post.body,
       customData: post.data.customData,
     })),
   });
